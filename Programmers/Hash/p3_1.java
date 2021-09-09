a348cb3ae335bf5f72d45d51d55b56be73285765
SlavesOf-821Ho <tablemin_park@daum.net>
2021-09-10 01:49:10 +0900

    Add Hash problem

--- Programmers/Hash/p3_1.java
+++ Programmers/Hash/p3_1.java
@@ -31,42 +31,42 @@ public class p3_1 {
         return answer;
     }
 
+    //처음에 gen.getOrDefault(genres[i], 0) 부분에서 gen.getOrDefault(genres[i], plays[i]) +  plays[i]
+    //였기 때문에 처음 연산값이 다르게 들어갔다. (key에 해당되는 객체가 없으면 0을 반환해야하는데)
+    //value(plays수)를 반환했기 때문에 첫값이 x2 되어 계산 되었다.
     public static int[] solution_2(String[] genres, int[] plays) {
         int[] answer = {};
 
-        HashMap<String, Integer> c = new HashMap<String, Integer>();
+        HashMap<String, Integer> gen = new HashMap<String, Integer>();
 
         for (int i = 0; i < genres.length; i++){
-            c.put(genres[i], c.getOrDefault(genres[i], plays[i]) + plays[i]);
+            gen.put(genres[i], gen.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-        List<String> cl = new ArrayList<>(c.keySet());
-        Collections.sort(cl, (o1, o2) -> c.get(o2).compareTo(c.get(o1)));
+        List<String> genList = new ArrayList<>(gen.keySet());
+        Collections.sort(genList, (o1, o2) -> gen.get(o2).compareTo(gen.get(o1)));
 
         List<Integer> ans = new ArrayList<>();
-
-        for (String gern : cl){
+        
+        for (String g : genList){
             List<Music> mList = new ArrayList<>();
             for (int i = 0; i < genres.length; i++){
-                if (genres[i].equals(gern)) mList.add(new Music(gern, plays[i], i));
-            }
-            Collections.sort(mList, (o1, o2) -> o2.play - o1.play);
-
-            for (Music m : mList) System.out.print(m.idx + " ");
-            System.out.println();
-
-            if (mList.size() == 1) 
-                ans.add(mList.get(0).idx);
-            else if (mList.size() > 1){
-                if (mList.get(0).play == mList.get(1).play){
-                    ans.add(mList.get(0).idx > mList.get(1).idx ? mList.get(1).idx : mList.get(0).idx);
-                    ans.add(mList.get(0).idx > mList.get(1).idx ? mList.get(0).idx : mList.get(1).idx);
-                }
-                else{
-                    ans.add(mList.get(0).idx);
-                    ans.add(mList.get(1).idx);
+                if (g.equals(genres[i])){
+                    mList.add(new Music(g, plays[i], i));
                 }
             }
+            Collections.sort(mList, new Comparator<Music>(){
+                @Override
+                public int compare(Music m1, Music m2){
+                    if (m1.play == m2.play){
+                        return m1.idx - m2.idx;
+                    }
+                    else return m2.play - m1.play;
+                }
+            });
+
+            ans.add(mList.get(0).idx);
+            if (mList.size() > 1) ans.add(mList.get(1).idx);
         }
 
         answer = ans.stream().mapToInt(Integer::intValue).toArray();
@@ -78,11 +78,12 @@ public class p3_1 {
         String[] t1_genres = {"classic", "pop", "classic", "classic", "pop"};
         int[] t1_plays = {500, 600, 150, 800, 2500};
         
-        String[] t2_genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] t2_plays = {400, 600, 500, 500, 2500};
+        String[] t2_genres = {"classic", "pop", "classic", "classic", "classic"};
+        int[] t2_plays = {500, 1000, 400, 300, 200};
 
-        //show(solution(t1_genres, t1_plays));
-        //show(solution_2(t1_genres, t1_plays));
+        show(solution(t1_genres, t1_plays));
+        show(solution_2(t1_genres, t1_plays));
+        
         show(solution_2(t2_genres, t2_plays));
     }
 
@@ -103,3 +104,49 @@ class Music{
         this.idx = idx;
     }
 }
+
+/**
+모범답안
+
+public class Solution {
+  public class Music implements Comparable<Music>{
+
+    private int played;
+    private int id;
+    private String genre;
+
+    public Music(String genre, int played, int id) {
+      this.genre = genre; 
+      this.played = played;
+      this.id = id;
+    }
+
+    @Override
+    public int compareTo(Music other) {
+      if(this.played == other.played) return this.id - other.id;
+      return other.played - this.played;
+    }
+
+    public String getGenre() {return genre;}
+  }
+
+  public int[] solution(String[] genres, int[] plays) {
+    return IntStream.range(0, genres.length)
+    .mapToObj(i -> new Music(genres[i], plays[i], i))
+    .collect(Collectors.groupingBy(Music::getGenre))
+    .entrySet().stream()
+    .sorted((a, b) -> sum(b.getValue()) - sum(a.getValue()))
+    .flatMap(x->x.getValue().stream().sorted().limit(2))
+    .mapToInt(x->x.id).toArray();
+  }
+
+  private int sum(List<Music> value) {
+    int answer = 0;
+    for (Music music : value) {
+      answer+=music.played;
+    }
+    return answer;
+  }
+}
+
+ */
