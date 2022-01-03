9bf5dd1371e2c48070c34d49994dbf3a2284dda7
ParkSangmin <tablemin_park@daum.net>
2022-01-03 23:21:54 +0900

    Add Normal Graph 1 (practice) problem

--- Baekjoon/Package/normal-graph 1 (practice)/p16929.java
+++ Baekjoon/Package/normal-graph 1 (practice)/p16929.java
@@ -28,34 +28,30 @@ public class p16929 {
         for (int i = 0; i < N; i++){
             for (int j = 0; j < M; j++){
                 v = new boolean[N][M];
-                if (DFS(i, j, i, j, map[i][j], 1)) {
-                    isCycle = true;
-                    break Loop;
-                }
+                DFS(i, j, i, j, map[i][j], 1);
+                if (isCycle) break Loop;
             }
         }
 
-        bw.write(isCycle ? "YES" : "NO");
+        bw.write(isCycle ? "Yes" : "No");
         bw.flush();
         br.close();
         bw.close();
     }
 
-    public static boolean DFS(int y, int x, int sy, int sx, int color, int depth){        
+    public static void DFS(int y, int x, int sy, int sx, int color, int depth){
         v[y][x] = true;
+        System.out.println(y + " " + x + " " + depth);
         for (int i = 0; i < 4; i++){
             int nextY = y + py[i];
             int nextX = x + px[i];
 
             if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= M || map[nextY][nextX] != color) continue;
-
-            if (!v[nextY][nextX]){
-                if (DFS(nextY, nextX, sy, sx, color, depth + 1)) return true;
-            }
-            else{
-                if (nextY == sy && nextY == sx && depth > 3)  return true;
-            }
+                        
+            if (nextY == sy && nextX == sx && depth > 3) isCycle = true;
+            
+            if (v[nextY][nextX]) continue;
+            DFS(nextY, nextX, sy, sx, color, depth + 1);                
         }
-        return false;
     }
 }
